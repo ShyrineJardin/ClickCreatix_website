392c7349c6e3d9b0b0e5921bed586deae59da0fd
ShyrineJardin <jardinshyrine@gmail.com>
2025-02-22 12:28:57 +0800

    updated font size

--- src/assets/components/Hero.jsx
+++ src/assets/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
   return (
     <section>
         <div className='flex flex-col items-center justify-center'>
-            <h1 className='mt-16  overflow-hidden text-[12vw] font-semibold font-oswa uppercase leading-none'>click creatix</h1>
+            <h1 className='mt-16  overflow-hidden text-[16vw] font-semibold font-oswa uppercase leading-none'>click creatix</h1>
 
             <div className='w-full'>
                 <img src={Eyes} alt="" className='mt-6 h-94 w-full object-cover'/>
